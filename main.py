4378a27865e4f1d20534ee882cf366e163d71991
Adam Siekierski <adam.siekiera@outlook.com>
2019-11-04 15:40:37 +0100

    Add two modes, bug fixes

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-import periodictable
+import json
+table = json.load(open('./periodic_table.json'))
 
-symbol = input('No daj symbol ziom: ')
-print(periodictable.elements.__getattribute__(symbol).mass)
+mode = input('Wybierz tryb (liczba atomowa - 1, symbol - 2): ')
+
+if (mode == '1'):
+    number = input('Podaj liczbę atomową: ')
+
+    for atom in table['elements']:
+        if (atom['number'] == int(number)):
+            foundAtom = atom
+            break
+    
+    try: foundAtom
+    except NameError: foundAtom = None
+    if (foundAtom == None):
+        print('Nie znaleziono takiego atomu')
+    else:
+        print(foundAtom['electron_configuration'])
+elif (mode == '2'):
+    symbol = input('Podaj symbol atomu: ')
+
+    for atom in table['elements']:
+        if (atom['symbol'] == symbol):
+            foundAtom = atom
+            break
+    
+    try: foundAtom
+    except NameError: foundAtom = None
+    if (foundAtom == None):
+        print('Nie znaleziono takiego atomu')
+    else:
+        print(foundAtom['electron_configuration'])
+else:
+    print('Nie istnieje tryb ', mode)
+
+
